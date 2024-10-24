a4f28e2f195c07f97151c59ca5218af4e6c68777
Ngo Tan An <ngotanan147@gmail.com>
2024-10-25 06:39:26 +0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,18 +24,26 @@ export class AppComponent {
   constructor(private loadingService: LoadingService) {}
 
   ngOnInit(): void {
+    let codeAndGroupDics: any[] = [];
     this.loadingService.showLoading();
     this._httpService
       .get(this.codeUrl)
       .pipe(
-        map((response: any) =>
-          response.data.flatMap((item: any) =>
-            item.codeList.map((subItem: any) => subItem.code),
-          ),
-        ),
-        switchMap((codeList: any) => {
+        map((response: any) => {
+          codeAndGroupDics = response.data.flatMap((item: any) =>
+            item.codeList.map((subItem: any) => {
+              return {
+                code: subItem.code,
+                group: item.vietnameseName,
+              };
+            }),
+          );
+          console.log(codeAndGroupDics);
+          return codeAndGroupDics;
+        }),
+        switchMap((res: any) => {
           return this._httpService.get(
-            `https://api-finfo.vndirect.com.vn/v4/financial_statements?q=code:${Array.from(codeList).join(',')}~reportType:QUARTER~modelType:2,90,102,412~fiscalDate:2024-12-31,2024-09-30,2024-06-30,2024-03-31,2023-12-31,2023-09-30,2023-06-30,2023-03-31,2022-12-31~itemCode:23003&sort=fiscalDate&size=1000`,
+            `https://api-finfo.vndirect.com.vn/v4/financial_statements?q=code:${Array.from(res.map((item: any) => item.code)).join(',')}~reportType:QUARTER~modelType:2,90,102,412~fiscalDate:2024-12-31,2024-09-30,2024-06-30,2024-03-31,2023-12-31,2023-09-30,2023-06-30,2023-03-31,2022-12-31~itemCode:23003&sort=fiscalDate&size=1000`,
           );
         }),
       )
@@ -43,6 +51,12 @@ export class AppComponent {
         next: (res: any) => {
           console.log(this.transformData(res.data));
           this.data = this.transformData(res.data);
+          this.data = this.data.map((item2) => {
+            const matchedItem = codeAndGroupDics.find(
+              (item1: any) => item1.code === item2.code,
+            );
+            return { ...item2, group: matchedItem?.group || 'Unknown' }; // Add group or 'Unknown' if not found
+          });
           this.loadingService.hideLoading();
         },
         error: () => {
@@ -79,6 +93,7 @@ export class AppComponent {
 
           return {
             code: code,
+            group: groupedData?.vietnameseName ?? null,
             value1:
               parseFloat((first.numericValue / 1000000000).toFixed(2)) || null, // First value
             value2:
